fd3c7885d25c91306179a3d3d4368229e69ef6c5
ShevArtV <shev.art.v@ya.ru>
2022-04-29 23:39:00 +0300

    Add settings message_handler and message_handler_method

--- ajaxform.class.php
+++ ajaxform.class.php
@@ -43,6 +43,8 @@ class AjaxForm
 
             'frontend_css' => $this->modx->getOption('af_frontend_css'),
             'frontend_js' => $this->modx->getOption('af_frontend_js'),
+            'message_handler' => $this->modx->getOption('af_message_handler') ?: 'SweetAlert2',
+            'message_handler_method' => $this->modx->getOption('af_message_handler_method') ?: 'Message',
         ), $config);
 
         $this->config['formSelector'] = $this->config['formSelector'] .'_'. rand();
